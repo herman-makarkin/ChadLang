b40614c979cfbc205c8a03675f7f779260318b36
Herman <german.makarkin@gmail.com>
2025-02-08 15:33:31 +0300

    add local variables

--- vm.c
+++ vm.c
@@ -164,9 +164,19 @@ static InterpretResult run()
         pop();
         break;
       }
+      case OP_SET_LOCAL: {
+        uint8_t slot = READ_BYTE();
+        vm.stack[slot] = peek(0);
+        break;
+      }
       case OP_NIL: push(NIL_VAL); break;
       case OP_TRUE: push(BOOL_VAL(true)); break;
       case OP_POP: pop(); break;
+      case OP_GET_LOCAL: {
+        uint8_t slot = READ_BYTE();
+        push(vm.stack[slot]);
+        break;
+      }
       case OP_GET_GLOBAL: {
       ObjString* name = READ_STRING();
       Value value;
